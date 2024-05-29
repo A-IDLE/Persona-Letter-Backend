0a52e45a7977545885bb0ef89836621d9a944d3f
inji <inji2349@gmail.com>
2024-05-29 12:35:47 +0900

    changed image status from hard coding to 0 to the real status

--- app/query/letter.py
+++ app/query/letter.py
@@ -104,40 +104,7 @@ def get_letters_by_reception_status(user_id: int, character_id: int, reception_s
                     "letter_id": letter.letter_id,
                     "character_name": character_name,
                     "letter_content": letter.letter_content,
-                    "letter_image_status": 0,
-                    "read_status": letter.read_status,
-                    "created_time": letter.created_time,
-                    # 추가적으로 필요한 Letter 필드를 여기에 포함시킬 수 있습니다.
-                }
-                result.append(letter_info)
-
-            return result
-    except Exception as e:
-        print("삐빅에러",e)  # 오류가 발생하면 오류 메시지를 출력합니다.
-        return None
-    
-def get_letters_by_reception_status(user_id: int, character_id: int, reception_status: str):
-    try:
-        with SessionLocal() as session:  
-            # Letter 테이블과 Character 테이블을 조인하고 필요한 정보를 선택합니다.
-            letters = session.query(
-                Letter, Character.character_name
-            ).join(
-                Character, Letter.character_id == Character.character_id
-            ).filter(
-                Letter.user_id == user_id, 
-                Letter.character_id == character_id,  # 클릭한 캐릭터 아이디와 일치하는 메일만 가져옵니다.
-                Letter.reception_status == reception_status
-            ).all()
-
-            # 결과를 딕셔너리 형태로 포맷하여 리스트에 추가합니다.
-            result = []
-            for letter, character_name in letters:
-                letter_info = {
-                    "letter_id": letter.letter_id,
-                    "character_name": character_name,
-                    "letter_content": letter.letter_content,
-                    "letter_image_status": 0,
+                    "letter_image_status": letter.letter_image_status,  # 실제 값을 사용
                     "read_status": letter.read_status,
                     "created_time": letter.created_time,
                     # 추가적으로 필요한 Letter 필드를 여기에 포함시킬 수 있습니다.
